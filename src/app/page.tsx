943cf1c4f079e9b3021247c7ca8349834cbc77c5
domesday <m.andriamiraho@gmail.com>
2024-01-09 18:54:35 +0100

    enhance: btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,25 @@ import Avatar from "@/components/avatar";
 import ContainerFullScreen from "@/components/container-full-screen";
 import Textarea from "@/components/ui/Textarea";
 
+import { DownloadCloud } from 'lucide-react';
 
 
 export default function HomePage() {
   return (
-    <main>
-      <ContainerFullScreen className="grid gap-y-5 place-content-center place-items-center">
+    <main className="snap-y">
+      <ContainerFullScreen className="relative grid gap-y-5 place-content-center place-items-center snap-center">
+        <Button className="absolute top-5 right-5"><DownloadCloud /></Button>
         <Avatar />
         <h1 className="text-3xl text-center">👋🏽Hi, my name is <br /><span className=" font-bold">Mathieu Andriamiraho</span></h1>
         <Button>Discover me ↓</Button>
       </ContainerFullScreen>
+      <ContainerFullScreen className="bg-black snap-center">
+        <p className="text-white">
+          I am a <span className="text-center text-white">Fullstack developer</span>
+        </p>
+      </ContainerFullScreen>
 
-      <ContainerFullScreen className="grid gap-y-5 place-content-center place-items-center">
+      <ContainerFullScreen className="grid gap-y-5 place-content-center place-items-center snap-center">
         <Input type="email" placeholder="youremail@here.fr" />
         <Textarea placeholder="Your message here" />
       </ContainerFullScreen>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,24 @@
-export default function Button({ children }: { children: React.ReactNode }) {
-    return(
-        <button className="threeD-btn">
-            {children}
-        </button>
-    )
+import cn from '@/utils/cn';
+import { Slot } from '@radix-ui/react-slot';
+import React from 'react';
+
+export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    asChild?: boolean
 }
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, asChild = false, ...props }, ref) => {
+        const Comp = asChild ? Slot : "button"
+        return (
+            <Comp
+                className={cn("threeD-btn", className)}
+                ref={ref}
+                {...props}
+            />
+        )
+    }
+)
+Button.displayName = "Button"
+
+export default Button;
